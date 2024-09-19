0f25347730cdc8f47280cebe8c00286e92835d9b
Polkovnik-Daniil <0205danik@gmail.com>
2024-09-19 13:16:36 +0300

    Добавьте файлы проекта.

---
+++ folder.checker.csharp/Program.cs
@@ -0,0 +1,35 @@
+﻿while (true)
+{
+    try
+    {
+        Console.Write("Enter folder path: ");
+        string? path = Console.ReadLine();
+        Console.Write("Enter file path:   ");
+        string? file = Console.ReadLine();
+        bool isExistFolder = Directory.Exists(path);
+        if (path == null || path == "" || !isExistFolder)
+        {
+            throw new Exception("Folder is not exists!");
+        }
+        if (file == null || file == "")
+        {
+            throw new Exception("File path is uncorrect!");
+        }
+        DirectoryInfo directory = new DirectoryInfo(path);
+        FileInfo[] Files = directory.GetFiles("*.*");
+        using (StreamWriter writer = File.AppendText(file))
+        {
+            foreach (FileInfo File in Files)
+            {
+                writer.WriteLine(File.Name);
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+    Thread.Sleep(10000);
+    Console.Clear();
+}
+
